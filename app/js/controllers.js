b5a0d97a8f4625b7064c10626143e1e46a2782c6
Arup Sen <arupsen48@gmail.com>
2014-09-05 12:09:53 +0100

    set up scope in waitlist controller and bound data with waitlist form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,5 +5,17 @@
 angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
   }])
-  .controller('WaitListController', [function() {
+  .controller('WaitListController', ['$scope', function($scope) {
+    $scope.parties = [];
+    $scope.party = {
+      name: '',
+      phone: '',
+      size: ''
+    }
+    $scope.saveParty = function(){
+      if ($scope.party){
+        $scope.parties.push($scope.party);
+        $scope.party = '';
+      }
+    }
   }]);
