21ce2972d577c4af2d73e8fe29113d73812356c1
Arup Sen <arupsen48@gmail.com>
2014-09-08 10:57:16 +0100

    set up controllers to create text message objects into new Firebase url

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,16 +6,25 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
   }])
   .controller('WaitListController', ['$scope','$firebase', function($scope, $firebase) {
-    var partiesRef = new Firebase('https://waitandeat-arup.firebaseio.com/');
+    // Connectiing $scope.parties to live Firebase data
+    var partiesRef = new Firebase('https://waitandeat-arup.firebaseio.com/parties');
 
+    // Object to store data from the waitlist form
     $scope.parties = $firebase(partiesRef);
     $scope.newParty = {
       name: '',
       phone: '',
       size: ''
     }
+    // Function to save a new party to the waitlist
     $scope.saveParty = function(){
         $scope.parties.$add($scope.newParty);
         $scope.newParty = '';
     }
+    // Function to send a text message to a party
+    $scope.sendTextMessage = function(phoneNumber) {
+      var textMessageRef = new Firebase('http://waitandeat-arup.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      textMessages.$add({phoneNumber: phoneNumber});
+    };
   }]);
